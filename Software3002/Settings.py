5940983043853d4f71a4c78e62d24f84eb1e79b8
huangkaiw3n <huangkaiw3n@gmail.com>
2014-11-15 08:58:05 +0800

    steplen 36

--- Software3002/Settings.py
+++ Software3002/Settings.py
@@ -20,6 +20,6 @@ offsetDistance = 50
 # Allowable difference in heading to considered as acceptable
 offSetAngle = 2.0
 # Step Length of user
-stepLength = 44.0
+stepLength = 36.0
 # Interval to report information
 reportInterval = 6.9
